d1aaf8db1f363d8555f1496a4df1c18ac4d410df
angelina de best <funformy@live.nl>
2022-11-20 14:53:26 +0100

    patrolling added with waypoints

--- Nunbeliever/Assets/Nun/Caregiver scripts/WalkingPath.cs
+++ Nunbeliever/Assets/Nun/Caregiver scripts/WalkingPath.cs
@@ -5,39 +5,53 @@ using UnityEngine;
 public class WalkingPath : MonoBehaviour
 {
     [SerializeField] private Transform[] waypointList;
+    [SerializeField] private int currentWaypoint = 0;
 
     [SerializeField] private float speed = 3;
 
-    private int currentWaypoint = 0;
     private Transform nextWaypoint;
 
     // Start is called before the first frame update
     void Start()
     {
-       transform.position =  waypointList[currentWaypoint].transform.position;
+       
        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+           if(nextWaypoint == null)
+            {
+                nextWaypoint = waypointList[currentWaypoint];
+                
+            }
+            Patrol();
     }
     void Patrol()
     {
         //Facing towards the destination
-        transform.forward = Vector3.RotateTowards(transform.forward, nextWaypoint.position -
-        transform.position, speed * Time.deltaTime, 0.0f);
+        
 
         //Moving towards the destination
         transform.position = Vector3.MoveTowards(transform.position, nextWaypoint.position,
         speed * Time.deltaTime);
 
         //When the agent reaches the waypoint it will move on to the next
-        if(transform.position == waypointList[currentWaypoint].transform.position)
+        if(transform.position.x == waypointList[currentWaypoint].transform.position.x &&
+           transform.position.z == waypointList[currentWaypoint].transform.position.z)
         {
-            currentWaypoint++;
-            nextWaypoint = waypointList[currentWaypoint];
+            
+            if (currentWaypoint < waypointList.Length - 1)
+            {
+                currentWaypoint++;
+                nextWaypoint = waypointList[currentWaypoint]; 
+            }
+            else
+            {
+                currentWaypoint = 0;
+                nextWaypoint = null;
+            }
         }
     }
 }
