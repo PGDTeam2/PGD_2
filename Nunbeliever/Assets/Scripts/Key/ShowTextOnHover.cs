1c85d5ba7182aa0a93fa10eb479d168bd5bba614
jEnS <jens1433@gmail.com>
2022-11-29 13:52:21 +0100

    Changed keyhover to universal hover
    Former-commit-id: fa0067aacd3fbb85e549b05a4f75851ac4d5effb

--- Nunbeliever/Assets/Scripts/Key/ShowTextOnHover.cs
+++ Nunbeliever/Assets/Scripts/Key/ShowTextOnHover.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public class Key : MonoBehaviour
+public class ShowTextOnHover : MonoBehaviour
 {
     private Animator m_animator;
 
