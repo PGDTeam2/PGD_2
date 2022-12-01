ad2035868b334e582e5cbda23e6f5c2ca8851e2f
jEnS <jens1433@gmail.com>
2022-12-01 11:44:46 +0100

    removed unnesecary includes
    Former-commit-id: 87394bf4dfe0a4e47bd8df3903da31031c320eee

--- Nunbeliever/Assets/Scripts/Player/HideMechanic.cs
+++ Nunbeliever/Assets/Scripts/Player/HideMechanic.cs
@@ -1,8 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor;
-using UnityEditor.PackageManager;
 using UnityEngine;
 
 public class HideMechanic : MonoBehaviour
