21e67a50314e3a55e986555262e612783f6bde41
angelina de best <funformy@live.nl>
2022-12-04 16:06:14 +0100

    fixes scene switching and lighting
    Former-commit-id: 93ca7436c437680727a70f5c0242d43d2ae93ee5

--- Nunbeliever/Assets/Scripts/Player/TPToOutside.cs
+++ Nunbeliever/Assets/Scripts/Player/TPToOutside.cs
@@ -6,30 +6,30 @@ using UnityEngine.UI;
 
 public class TPToOutside : MonoBehaviour
 {
-    private GameObject m_spawnPoint;
     private GameObject m_whiteOutPanel;
-    private GameObject m_outsideLight;
-    private GameObject m_player;
-
+    [SerializeField] string m_scene;
+    [SerializeField] int r;
+    [SerializeField] int g;
+    [SerializeField] int b;
     private void Awake()
     {
-        if (!SceneManager.GetSceneByName("Demo").isLoaded)
-            SceneManager.LoadSceneAsync("Demo", LoadSceneMode.Additive);
+        /*if (!SceneManager.GetSceneByName("Demo").isLoaded)
+            SceneManager.LoadSceneAsync("Demo", LoadSceneMode.Additive);*/
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("player"))
         {
-            m_spawnPoint = GameObject.FindWithTag("OutsideSpawn");
+            //m_spawnPoint = GameObject.FindWithTag("OutsideSpawn");
             m_whiteOutPanel = GameObject.FindWithTag("WhiteOutPanel");
-            m_outsideLight = GameObject.FindWithTag("OutsideLight");
-            m_player = other.gameObject;
-            StartCoroutine(DoTransition());
+            //m_outsideLight = GameObject.FindWithTag("OutsideLight");
+            //m_player = other.gameObject;
+            StartCoroutine(DoTransition(m_scene, r, g, b));
         }
     }
 
-    IEnumerator DoTransition()
+    IEnumerator DoTransition(string scene,int r, int g, int b)
     {
         var image = m_whiteOutPanel.GetComponent<Image>();
 
@@ -38,22 +38,23 @@ public class TPToOutside : MonoBehaviour
         while (t < 1f)
         {
             t += 0.01f;
-            image.color = new Color(1f, 1f, 1f, t);
+            image.color = new Color(r, g, b, t);
             yield return null;
         }
+        SceneManager.LoadScene(scene);
 
         // Set Player transform
-        m_player.transform.position = m_spawnPoint.transform.position;
-        m_player.transform.rotation = m_spawnPoint.transform.rotation;
+        //m_player.transform.position = m_spawnPoint.transform.position;
+       // m_player.transform.rotation = m_spawnPoint.transform.rotation;
 
         // Switch lighting and scene
-        SceneManager.SetActiveScene(SceneManager.GetSceneByName("Demo"));
-        m_outsideLight.GetComponent<Light>().enabled = true;
+       // SceneManager.SetActiveScene(SceneManager.GetSceneByName("Demo"));
+        //m_outsideLight.GetComponent<Light>().enabled = true;
 
         yield return new WaitForSeconds(0.1f);
 
         // Fade out white screen
-        while (t > 0f)
+        /*while (t > 0f)
         {
             t -= 0.01f;
             image.color = new Color(1f, 1f, 1f, t);
@@ -62,22 +63,7 @@ public class TPToOutside : MonoBehaviour
         if(t < 0f)
         {
           StartCoroutine(wakeUp());
-        }
+        }*/
     }
-    IEnumerator wakeUp()
-    {
-        yield return new WaitForSeconds(3);
-        var image = m_whiteOutPanel.GetComponent<Image>();
 
-        // Fade in white screen
-        float t = 0;
-        while (t < 1f)
-        {
-            t += 0.1f;
-            image.color = new Color(0f, 0f, 0f, t);
-            yield return null;
-        }
-        if(t > 1f)
-        SceneManager.LoadSceneAsync("Nun");
-    }
 }
