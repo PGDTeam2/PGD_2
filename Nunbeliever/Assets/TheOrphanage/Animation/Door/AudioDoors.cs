074c402d96ec57ffe32bee18aed64b3548ef223d
angelina de best <funformy@live.nl>
2022-12-18 15:40:06 +0100

    files changes
    Former-commit-id: f049626880dc83d879005a0768aacd4a0459d650

---
+++ Nunbeliever/Assets/TheOrphanage/Animation/Door/AudioDoors.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioDoors : MonoBehaviour
+{
+    public AudioClip Open;
+    public AudioClip Close;
+    AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
