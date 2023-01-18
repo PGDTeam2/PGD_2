5bf6eb32b0e96647697d1bb85d41acda4efc00eb
Kasper <kasper.krajewski@hva.nl>
2023-01-18 19:04:35 +0100

    End scene doneza

--- Nunbeliever/Assets/Scripts/LevelFades/FadeOut.cs
+++ Nunbeliever/Assets/Scripts/LevelFades/FadeOut.cs
@@ -26,6 +26,7 @@ public class FadeOut : MonoBehaviour
     
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log(other.name);
         if (other.CompareTag("Player"))
         {
             m_whiteOutPanel = GameObject.FindWithTag("WhiteOutPanel");
